eae457c151cee35fbd84b10555d2fb13d303ea70
iay <septianadip7998@gmail.com>
2024-05-07 01:32:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataApi: [],
+      dataPost: {
+        id: "",
+        title: "",
+        body: "",
+      },
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.inputChange = this.inputChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  reloadData() {
+    axios.get("http://localhost:3004/posts").then((res) => {
+      this.setState({ dataApi: res.data });
+    }); //menggunakan axios
+  }
+  handleRemove(e) {
+    const idToDelete = Number(e.target.value);
+    console.log(idToDelete);
+    fetch(`http://localhost:3004/posts/${idToDelete}`, {
+      method: "DELETE",
+    }).then((res) => this.reloadData());
+  }
+  
+
+  inputChange(e) {
+    let newdataPost = { ...this.state.dataPost };
+    newdataPost["id"] = new Date().getTime();
+    newdataPost[e.target.name] = e.target.value;
+
+    this.setState(
+      {
+        dataPost: newdataPost,
+      },
+      () => console.log(e.target.value)
+    );
+  }
+
+  onSubmitForm() {
+    const id = this.state.dataPost.id.toString();
+    axios.post(`http://localhost:3004/posts/`, { ...this.state.dataPost, id }).then(() => {
+      this.reloadData();
+    });
+  }
+  
+
+  componentDidMount() {
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(res =>
+    //     this.setState({ dataApi: res
+    //     })
+    // ) //menggunakan fetch
+
+    // axios.get("http://localhost:3004/posts").then((res) => {
+    //   this.setState({ dataApi: res.data });
+    // }); //menggunakan axios
+
+    this.reloadData();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Halo Api</h1>
+        <input
+          type="text"
+          name="body"
+          placeholder="Masukkan Body"
+          onChange={this.inputChange}
+        />
+        <input
+          type="text"
+          name="title"
+          placeholder="Masukkan Title"
+          onChange={this.inputChange}
+        />
+        <button type="submit" onClick={this.onSubmitForm}>
+          Add Data
+        </button>
+        {this.state.dataApi.map((data, index) => {
+          return (
+            <div key={index} style={{ padding: "0", margin: "0" }}>
+              <p>{data.id}</p>
+              <p>{data.title}</p>
+              <p>{data.body}</p>
+              <button value={data.id} onClick={this.handleRemove}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
